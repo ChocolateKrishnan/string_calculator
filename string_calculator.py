34e94c62b5c6df0e6d62bb7a9cdad7b5ac423586
Gokul Krishnan <69914624+ChocolateKrishnan@users.noreply.github.com>
2022-05-04 10:13:58 +0530

    Minor Bug Change

--- string_calculator.py
+++ string_calculator.py
@@ -1,23 +1,32 @@
 def Add(string_number_list):
     delimiter=","
     if string_number_list.startswith("//"):
-        delimiter=string_number_list[2]
-        string_number_list=string_number_list[5:]
+        string_number_list=string_number_list[2:]
+        string_number_list=string_number_list.split("\\n")
+        delimiter=string_number_list[0]
+        new_string_list=string_number_list[1:]
+        string_number_list=''
+        string_number_list=string_number_list.join(new_string_list)
     string_number_list=string_number_list.split(delimiter)
     number_sum=0
     for i in string_number_list:
-        if i.isnumeric():
+        try:
+            if i=='':
+                i=0
             if int(i)<0:
                 raise Exception("negatives not allowed")
-            number_sum+=int(i)
-        else:
+            else:
+                number_sum+=int(i)
+        except:
             if i.find("\\n"):
                 i_list=i.split("\\n")
+                print(i_list)
                 for j in i_list:
-                    if j.isnumeric():
-                        if int(j)<0:
-                            raise Exception("negatives not allowed")
-                        number_sum+=int(j)  
+                    if j=='':
+                        j=0
+                    if int(j)<0:
+                        raise Exception("negatives not allowed")
+                    number_sum+=int(j)  
     return number_sum
 print("Enter the String Argument")
 string_number_list=input()
